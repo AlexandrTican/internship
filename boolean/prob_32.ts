ce05be1ccd2d1877bd8b63d4ee374b7118c274e3
AlexandrTican <alexandr.tican@iis.utm.md>
2022-09-09 17:52:43 +0300

    why u boolean me

---
+++ boolean/prob_32.ts
@@ -0,0 +1,6 @@
+/*Boolean32. Given three integers a, b, c that are the sides of a triangle, verify the
+following proposition: ―The triangle with sides a, b, c is a right triangle‖*/
+let a : number = 3;
+let b : number = 4;
+let c : number = 5;
+console.log(Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2));
---
+++ boolean/prob_40.ts
@@ -0,0 +1,3 @@
+/*Boolean40. Given coordinates x1, y1, x2, y2 of two chessboard squares (as integers in
+the range 1 to 8), verify the following proposition: ―A knight can move from
+one square to another during one turn‖*/
