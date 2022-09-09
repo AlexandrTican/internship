016c72d36c8604d0730ac5683878e8dd3e3eb60c
AlexandrTican <alexandr.tican@iis.utm.md>
2022-09-09 16:35:33 +0300

    more stuff

---
+++ integers/prob_21.ts
@@ -0,0 +1,5 @@
+/*Integer21. From the beginning of the day N seconds have passed (N is integer). Find
+an amount of seconds passed from the beginning of the last minute.*/
+let secondsPassed: number = 5105;
+let lastMinute: number = secondsPassed % 60;
+console.log(lastMinute);
