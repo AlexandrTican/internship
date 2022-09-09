94d02d1dbfdf805fa1d45f52b015523d4e778831
AlexandrTican <alexandr.tican@iis.utm.md>
2022-09-09 17:05:31 +0300

    why you boolean me

---
+++ boolean/prob4.ts
@@ -0,0 +1,5 @@
+/*Boolean4. Given two integers A and B, verify the following proposition: ―The
+inequalities A > 2 and B ≤ 3 both are fulfilled */
+let a : number = 105;
+let b : number = 4;
+console.log( a > 2 === b <= 3);
