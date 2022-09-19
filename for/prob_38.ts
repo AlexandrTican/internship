2b233b7e38679c49eb227301b810b718f237b939
AlexandrTican <alexandr.tican@iis.utm.md>
2022-09-19 14:00:10 +0300

    j

--- for/prob_38.ts
+++ for/prob_38.ts
@@ -2,3 +2,15 @@
 1^N+ 2^N−1 + … + N^1.
 To avoid the integer overflow, compute the sum using real variables and
 output the result as a real number.*/
+
+let n : number = 5;
+let sum : number = 0;
+let value : number = 0;
+for( let i = 0; i < n; i++){
+    value++
+    //console.log(value)
+    for (let j = 0; j < n; j++){
+        sum += Math.pow(value,n--)
+        console.log(sum);
+    }
+}
