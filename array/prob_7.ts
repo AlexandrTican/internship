353d782e3b29ad35998f7e6c616798a4cf127b91
AlexandrTican <alexandr.tican@iis.utm.md>
2022-09-22 10:22:43 +0300

    Update prob_7.ts

--- array/prob_7.ts
+++ array/prob_7.ts
@@ -1,13 +1,15 @@
 /*Array7. Given an array of N real numbers, output its elements in inverse order. */
 
 
-function inverse(list:number[]):number{
+/*function inverse(list:number[]):number[]{
     for(const value of list){
         
     }
-}
+    return list;
+}*/
 
 
 
 
-const x : number[] = [];
+const x : number[] = [10,20,50,100]
+console.log(x);
