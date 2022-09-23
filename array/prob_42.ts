b3ca3dd5355bc2595f669100df62ef068cc8f089
AlexandrTican <alexandr.tican@iis.utm.md>
2022-09-23 17:02:19 +0300

    smth

--- array/prob_42.ts
+++ array/prob_42.ts
@@ -3,6 +3,14 @@ adjacent elements whose sum of values is the nearest to the number R and
 output these elements in ascending order of its indices (see the definition of
 two nearest numbers in Array40).an element AK such that the value |AK − R| is minimal)*/
 
-function  nearSum(R : number , list:number[]){
-    
+function  nearSum(R : number , list:number[]):number[]{
+    const sumArray :number[] = [];
+    for(const value of list){
+        list[value] += list[value+1];
+        sumArray.push(list[value]);
+    }
+    return sumArray;
 }
+const arr_test :number[] = [1 ,3 ,5 ,6, 7, 8, 9, 11]
+let R2 : number = 10;
+console.log(nearSum(R2,arr_test));
