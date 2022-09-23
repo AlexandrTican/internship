714704c43ddc6205abdf40ee0d2370b96b282b98
AlexandrTican <alexandr.tican@iis.utm.md>
2022-09-23 16:14:50 +0300

    yay

--- array/prob_35.ts
+++ array/prob_35.ts
@@ -3,37 +3,34 @@ maximums (see the local maximum definition in Array33).*/
 
 function minLMAX(list:number[]){
     const localMax: number[] =[];
-    for(let i = 1 ; i < list.length ; i++){
-        // if(list[i] > list[i-1] && list[i] > list[i+1]){
-        //     localMax = [list[i]];
-        //     console.log(localMax);
-        // }
+    for(let i = 0 ; i < list.length ; i++){
+
         if (i === 0 && list[i] > list[i+1]) {
             localMax.push(list[i]);
+            continue;
         }
+
+        if( i === list.length - 1 && list[i] > list[i-1]){
+            localMax.push(list[i]);
+            continue;
+        }
+
         if(list[i] > list[i+1] && list[i] > list[i-1]){
             localMax.push(list[i])
         }
     }
+
     console.log(localMax);
+
     let min : number = Number.MAX_SAFE_INTEGER;
+    
     for(const value of localMax){
         if(value < min){
             min = value;
         }
     }
-    // for(let i = 0; i < localMax.length;i++){
-    //     min = localMax[i];
-    //     if(localMax[i] < min){
-    //         min = localMax[i]
-    //     }
-    return min;
+     return min;
     }
-  
-    //     if(list[i] > list[i-1] && list[i] > list[i+1]){
-    //     localMax.push(list[i])
-    //     console.log(localMax);
-    // }
 
 
 const arr: number[] = [3, 2, 0, 4, 3, 6, 5, 8, 9];
--- array/prob_42.ts
+++ array/prob_42.ts
@@ -1,4 +1,8 @@
 /*Array42. A real number R and an array of N real numbers are given. Find two
 adjacent elements whose sum of values is the nearest to the number R and
 output these elements in ascending order of its indices (see the definition of
-two nearest numbers in Array40).*/
+two nearest numbers in Array40).an element AK such that the value |AK − R| is minimal)*/
+
+function  nearSum(R : number , list:number[]){
+    
+}
