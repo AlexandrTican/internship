5c320ebefd0669f393c4da6c94e231468d29cc02
AlexandrTican <alexandr.tican@iis.utm.md>
2022-09-23 13:53:59 +0300

    yay

--- array/prob_14!!!.ts
+++ array/prob_14!!!.ts
@@ -4,12 +4,18 @@ with odd order numbers (in ascending order of indices too):
 A2, A4, A6, …, A1, A3, A5, … .
 Do not use conditional statements. */
 
-function oddArray(list:number[]):void
+function evenArray(list:number[]):void
 {
     for(let i = 0 ; i < list.length; i+=2){
         console.log(list[i]);
     }
 }
+function oddArray(list:number[]):void{
+    for(let i = 1; i < list.length; i+=2){
+        console.log(list[i]);
+    }
+}
 
 const K : number[] = [10 , 15, 20, 25, 30 , 35, 40, 45, 50];
+console.log(evenArray(K));
 console.log(oddArray(K));
--- array/prob_28!!!.ts
+++ array/prob_28!!!.ts
@@ -1,12 +1,17 @@
 /*Array28. Given an array A of N real numbers, find the minimum among elements
 with even order numbers: A2, A4, A6, … .*/
 
-function minEven(list:number[],min)
-{
-    for(let i = 0 ; i < list.length-1 ; i++)
-    {
-        if(list[i] % 2 === 0) {
-        }
+function minEven(list:number[]):number
+{   let min : number = 0;
+    for(let i = 0 ; i < list.length ; i+=2){
+        min = list[i]
+        if(min < list[i]){
+            min =list[i]
+        };
     }
+    return min;
 }
+
+let A : number[] = [10  ,  9 , 8 , 7 ,6 ,5 ,4 ,3 ,2, 1];
+console.log(minEven(A));
 
