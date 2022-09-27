30ffa1db8da8a221f3c4a2500dce0d74968c38dd
AlexandrTican <alexandr.tican@iis.utm.md>
2022-09-27 14:14:51 +0300

    Update prob_70.ts

--- array/prob_70.ts
+++ array/prob_70.ts
@@ -1,2 +1,10 @@
 /*Array70. Given an array of N real numbers (N is an even number), exchange values
 of the first half and the second half of its elements. */
+
+function splitHalf(arr:number[]):number[]{
+    if((arr.length-1) % 2 != 0){
+        console.log("Your array does not contain an even amount of values");
+        return;
+    }
+    
+}
