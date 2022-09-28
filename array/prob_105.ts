9b2abc6e457b6df3daa51f37901412faa180ac03
AlexandrTican <alexandr.tican@iis.utm.md>
2022-09-28 13:51:55 +0300

    prob105 solved, working on 112

--- array/prob_105.ts
+++ array/prob_105.ts
@@ -3,9 +3,19 @@
 //     with the order number K. 
 
 function insertGarbage(arr:number[], K:number, M:number):number[] {
-    for( let i = K; i <arr.length; i++){
-        arr.splice(i,0,0)
+ 
+    for ( let i = 0; i < arr.length; i++){
+        if(arr[i]===K){
+            while( M > 0){
+                arr.splice(i,0,0);
+                M--
+            }
+        }
     }
-
     return arr;
 }
+
+const N : number[] = [1,2,3,4,5,6,7,8,9,10]
+let M : number = 3;
+let K : number = 5;
+console.log(insertGarbage(N,K,M));
---
+++ array/prob_112.ts
@@ -0,0 +1,13 @@
+/*Array112. An array A of N (≤ 6) real numbers is given. Sort the array in ascending
+order by using the exchange sort method (the bubble sorting): compare each
+pair of two adjacent elements (A1 and A2, A2 and A3, and so on) and exchange
+their values in case the left element is greater than the right one; repeat N − 1
+times such array pass. Output all array elements after each pass for checking
+results. Note that the amount of analyzed pairs of the array elements can be
+reduced by 1 after each pass*/
+
+// function swap(arr:number[],xp,yp)   
+function bubbleSort(arr:Array<number>):any[]{
+    
+    return arr;
+}
