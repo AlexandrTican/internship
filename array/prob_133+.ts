d131206ac8eb36340c105f1c8ffa3447c5b58633
AlexandrTican <alexandr.tican@iis.utm.md>
2022-10-04 17:45:10 +0300

    fixed 133 and 30 , can do further research

---
+++ array/prob_133+.ts
@@ -0,0 +1,68 @@
+/*Array133. A set A of N points in the coordinate plane is given (points are determined
+by their coordinates x, y). Find the point of A that lies in the first or the third
+coordinate quarter and is the nearest to the origin. If the set A does not contain
+such points then output the origin (0, 0). */
+// Each set of points must be stored either in two arrays of numbers (the first
+//     array stores x-coordinates, the second one stores y-coordinates) or in one array of
+//     records with two fields x and y.
+
+/* IMPORTANT !!!!!!!!! = first and third quarter means ++ or --  : so values in x , y arrays must be ++ or -- */
+//and closest element to 0;
+function pointZero(arr: [number, number][]){
+    //array in care se plaseaza distantele de la puncte la origine
+    // let distance  : Array<number> =[];
+    const array : [number, number][] = [];
+    for( let i = 0 ; i < arr.length ; i++) {
+        const x = arr[i][0], y = arr[i][1];
+        if(Math.sign(x) === Math.sign(y)){
+            array.push(arr[i]);
+        }
+    }
+    if(array.length === 0) { 
+        return [0,0];
+    }
+    
+    let result = array[0];
+    let distance = Math.sqrt(result[0] ** 2 + result[1] ** 2);
+
+    for (let i = 1; i < array.length; i++) {
+        const item = array[i];
+        const itemDisatnce = Math.sqrt(item[0] ** 2 + item[1] ** 2);
+        if (itemDisatnce < distance) {
+            result = item;
+            distance = itemDisatnce;
+        }
+    }
+
+    return result;
+    // for ( let i = 0 ; i < arr.length; i++){
+//         let x  = arr[i][0], y= arr[i][1];
+//         if((x >= 0 && y>= 0) || (x<= 0 && y>=0)){
+//         distance[i] = (x*x) + (y*y);
+//     }
+//     }
+//     distance.sort(function(a,b){return a-b;});
+
+//     console.log("Distance array" , distance);
+//     for(let i = 0; i < arr.length; i++)
+//     {
+//         let x = arr[i][0], y= arr[i][0];
+//         let dist = x*x + (y*y);
+//         if(dist===distance[0]){
+//         console.log(x,y);
+//         }
+//     }
+
+//     // for(let i = 0; i < arr.length;i++){
+//     //     let x = arr[i][0], y=arr[i][1];
+//     //     let dist = (x*x) + (y*y);
+//     // }
+}
+const A : [number,number][] = [[1,2],[-1,3],[0,0],[1,1]];
+console.log(pointZero(A));
+    //         if((x[0] >= 0 && y[0]>= 0) || (x[0]<= 0 && y[0]>=0)){
+    //            return console.log("The pair of coordinates closest to the origin is ",(x[0],y[0]));
+    //         } else{
+    //             return console.log("There are no suitable elements")
+    //         }
+    // }
---
+++ matrix/prob_30+.ts
@@ -0,0 +1,44 @@
+/*Matrix30. An M × N matrix of real numbers is given. For each matrix column find
+the amount of elements that are greater than the average of all elements of this
+column. */  
+
+function avgCompare(matrix:number[][]){
+    for( let i = 0 ; i < matrix[0].length; i++) {
+        let avg = 0;
+        let counter = 0;
+        console.log("column");
+        for( let j = 0 ; j < matrix.length;j++) {
+            console.log(matrix[j][i]);
+            avg += matrix[j][i]
+            counter++
+        }
+        avg = avg / counter
+        for(let i = 0; i < matrix[0].length;i++) {
+            for (let j = 0; j < matrix.length;j++) {
+                if(matrix[j][i] > avg) {
+                    console.log("A bigger than average element is ",matrix[j][i])
+                }
+            }
+        };
+        //add a count , another for(double), bigger 
+                console.log("avg for this column is",avg);
+    }
+}
+// function avgFinder(matrix:number[][]){
+//     let avg : number = 0;
+//     // push avg of each column in an array... go through initial array again and compare with avg of column
+//     // for ( let rowIndex = 0 ; rowIndex < matrix.length; rowIndex++)
+//      for ( let rowIndex = 0; rowIndex < matrix.length;rowIndex++){
+//         const row = matrix[rowIndex];
+//         // for( let colIndex =0 ; colIndex < row.length; colIndex++) 
+//         for(let colIndex=0; colIndex < row.length;colIndex++){
+//             // avg += row[colIndex];
+//             console.log(row[colIndex]);
+//         }
+//     }
+//     // console.log("There are ",matrix.length ,'\b',"elements in the matrix");
+//     // avg = avg / (matrix.length-1)
+// }
+
+const testMatrix_2 : number[][] = [[1,2,3],[3,4,5],[5,6,7],[7,8,9],[9,10,11]];
+console.log(avgCompare(testMatrix_2))
