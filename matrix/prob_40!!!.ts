3b9ad3153138a62e372ba0df4982899671e4dafd
AlexandrTican <alexandr.tican@iis.utm.md>
2022-10-05 12:26:45 +0300

    fixing 40

--- matrix/prob_40!!!.ts
+++ matrix/prob_40!!!.ts
@@ -5,15 +5,15 @@ function equalMax(matrix:number[][]){
     let result : number[] =[];
     let counter : number = 0;
     for(let rowIndex = 0; rowIndex < matrix.length; rowIndex++){
-        //push amount of equal elements in array;
         const row = matrix[rowIndex];
-        result.push(counter);
-        //counter is turned to 0;
+        //counter is reset to 0 for next row;
         counter = 0;
         for(let colIndex = 0; colIndex < row.length; colIndex++){
             if(row[colIndex]===row[colIndex+1]){
                 counter++
             }
         }
+        //push amount of equal elements into array;
+        result.push(counter);
     }
 }
