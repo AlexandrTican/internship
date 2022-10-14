59d13b2c6a76612fef6b5991c38f1eb09781a15b
AlexandrTican <alexandr.tican@iis.utm.md>
2022-10-14 18:45:17 +0300

    practice factory

--- OOP/factory_practice.ts
+++ OOP/factory_practice.ts
@@ -1,24 +1,65 @@
-function Developer(name) {
-    this.name = name;
-    this.type = "Developer"
-}
-function Tester(name) {
-    this.name = name;
-    this.type = "Tester";
-}
-function EmployeeFactory() {
-    this.create = (name:number,type:number) => {
-        switch(type) {
-            case 1:
-                return new Developer(name);
-                break;
-            case 2:
-                return new Tester(name);
-                break;
-        }
-    }
-}
-const employeeFactory = new EmployeeFactory();
-const employees = [];
+// function Developer(name:string) {
+//     this.name = name;
+//     this.type = "Developer"
+// }
+// function Tester(name:string) {
+//     this.name = name;
+//     this.type = "Tester";
+// }
+// function EmployeeFactory() {
+//     this.create = (name:number,type:number) => {
+//         switch(type) {
+//             case 1:
+//                 return new Developer(name);
+//                 break;
+//             case 2:
+//                 return new Tester(name);
+//                 break;
+//         }
+//     }
+// // }
+// // const employeeFactory = new EmployeeFactory();
+// const employees = [];
 
-employees.push(employeeFactory.create("Patrick",1));
+// employees.push(employeeFactory.create(1,1));
+enum Department {
+    Engineering = 'engineering',
+    Marketing = 'marketing',
+    Accounting = 'accounting',
+}
+interface IEmployee {
+    getDepartment(): Department
+    getSalary() : number;
+    readonly name:  string;
+}
+class MarketingManager implements IEmployee {
+    getDepartment(): Department {
+        return Department.Marketing;
+    }
+    getSalary(): number{
+        return 75000;
+    }
+    name: 'Mike Hunt'
+}
+class SoftwareEngineer implements IEmployee {
+    getDepartment(): Department {
+        return Department.Engineering;
+    }
+    getSalary(): number{
+        return 90000;
+    }
+    name: 'Mike Hawk'
+}
+class Accounting implements IEmployee {
+    getDepartment(): Department {
+        return Department.Accounting;
+    }
+    getSalary(): number{
+        return 80000;
+    }
+    name: 'Ben Dover'
+}
+class OrganizationFactory {
+
+    constructor(name: string) { }
+}
