2ce65f1e70d65e0f25b0abe935073339fde2aca0
AlexandrTican <alexandr.tican@iis.utm.md>
2022-10-14 17:41:34 +0300

    task 4 done

--- OOP/prob_4.ts
+++ OOP/prob_4.ts
@@ -24,5 +24,45 @@ class SNode<T> {
 }
 
 class Stack<T> {
-    constructor() {}
+    private _head: SNode<T> | null;
+    private _size: number;
+    constructor() {
+        this._head = null;
+        this._size = 0;
+    }
+    
+    public push(data: T):void {
+        let newNode = new SNode(data,null);
+        if(this._head === null) {
+            this._head = newNode;
+        } else { 
+            newNode.next = this._head;
+            this._head = newNode;
+        }
+        this._size++;
+    }
+    
+    public pop():T | null { 
+        if(this._head === null) return null;
+        let temp = this._head;
+        this._head = temp.next;
+        this._size--;
+        return temp.data;
+    }    
+    get size():number { 
+        return this._size;
+    }
 }
+const numberStack = new Stack<number>();
+numberStack.push(1); // Nodes [1] -> Head 1
+numberStack.push(2); // Nodes [2, 1] -> Head 2
+numberStack.push(3); // Nodes [3, 2, 1] -> Head 3
+numberStack.push(4); // Nodes [4, 3, 2, 1] -> Head 4
+numberStack.push(5); // Nodes [5, 4, 3, 2, 1] -> Head 5
+console.table(numberStack);
+
+numberStack.pop(); // Nodes [4, 3, 2, 1] -> Head 4
+// Output 5
+numberStack.pop(); // Nodes [3, 2, 1] -> Head 3
+console.log(numberStack);
+console.table(numberStack);
