699e8874738c1e4a230b908959fc6e5c5cb5f23c
AlexandrTican <alexandr.tican@iis.utm.md>
2022-10-15 23:27:12 +0300

    added cylinder spinning

--- fun stuff/minigame.ts
+++ fun stuff/minigame.ts
@@ -3,21 +3,22 @@ let max = Math.floor(6);
 let HP = 100;
 let turn = 1;
 //gets an integer between min and max, inclusive
-let chamberRound = Math.floor(Math.random() * (max-min + 1) + min);
+//load a bullet into the revolver
+let loadBullet = Math.floor(Math.random() * (max-min + 1) + min);
 
 function shoot(){
-    while(HP != 0) { 
-     
+    //where the hammer is currently at 
     let hammer = Math.floor(Math.random() * (max-min+1) +min);
-    if(hammer === chamberRound) { 
+    while(HP != 0) { 
+    if(hammer === loadBullet) { 
         console.log("Rest in peace, asshole");
         HP = 0;
-        // console.log(`round in chamber ${chamberRound}`)
+        // console.log(`round in chamber ${loadBullet}`)
         // console.log(`hammer on chamber ${hammer}`)
     }
         else {
         console.log("You're alive , for now")
-        // console.log(`round in chamber ${chamberRound}`)
+        // console.log(`round in chamber ${loadBullet}`)
         // console.log(`hammer on chamber ${hammer}`)
         hammer--
         if(hammer === 1) {
