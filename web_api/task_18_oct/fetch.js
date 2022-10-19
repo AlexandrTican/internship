a4830021612004875d97f34abd32b849bea69685
AlexandrTican <alexandr.tican@iis.utm.md>
2022-10-19 08:54:21 +0300

    search bar implemented, functional

--- web_api/task_18_oct/fetch.js
+++ web_api/task_18_oct/fetch.js
@@ -43,3 +43,17 @@ function mark(){
   var obj = new Mark(context);
 }
 
+function search() {
+  let input = document.getElementById('searchbar').value
+  input=input.toLowerCase();
+  let x = document.getElementsByTagName('li');
+    
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="list-item";                 
+      }
+  }
+}
