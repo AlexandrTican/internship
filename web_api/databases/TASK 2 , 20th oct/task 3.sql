8a994389ae27e6749e5e008f729b4361b049c565
AlexandrTican <alexandr.tican@iis.utm.md>
2022-10-20 16:26:20 +0300

    task 3 WIP

---
+++ web_api/databases/TASK 2 , 20th oct/task 3.sql
@@ -0,0 +1,16 @@
+INSERT into
+	"ClientInfo"(country,city,address)
+VALUES
+	('USA','Detroit','Hickory Lane st. 141'),
+	('Japan','Osaka','street2'),
+	('Germany','Hamburg','street3'),
+	('Ukraine','Kiev','street512'),
+	('Finland','Turku','street32'),
+	('Romania','Iasi','street311'),
+	('Moldova','Chisinau','street119'),
+	('USA','Michigan','street31'),
+	('Japan','Hokkaido','street4'),
+	('Spain','Marid','street51'),
+	('Italy','Rome','street951'),
+	('France','Paris','street33'),
+	('Thailand','Bangkok','street30');
