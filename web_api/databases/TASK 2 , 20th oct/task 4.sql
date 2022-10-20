5d1f25f9d29d2c5ba96b9d4d4e7238175c079be9
AlexandrTican <alexandr.tican@iis.utm.md>
2022-10-20 23:37:50 +0300

    task 4 done

--- web_api/databases/TASK 2 , 20th oct/task 4.sql
+++ web_api/databases/TASK 2 , 20th oct/task 4.sql
@@ -1,2 +1,43 @@
 DELETE from "ClientInfo"
 WHERE country = 'Thailand';
+
+SELECT
+	item_name,
+	item_description
+FROM
+	"Items"
+ORDER BY
+	item_name
+FETCH FIRST 10 ROW ONLY;
+
+
+SELECT
+	item_name,
+	item_description
+FROM
+	"Items"
+ORDER BY
+	item_name
+OFFSET 5 ROWS
+FETCH LAST 5 ROW ONLY;
+
+
+SELECT * from "Orders"
+
+WHERE amount between 99 and 100000;
+
+
+SELECT 
+	username,
+	firstname,
+	lastname
+FROM
+	"Clients"
+WHERE
+	lastname like 'D%';
+
+UPDATE "Orders"
+
+SET order_date = '2010-04-29'
+
+WHERE id = 'f03e7836-a976-440b-88f7-f52adb1fa4c7'
